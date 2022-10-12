2daa106052d08b15420e23110b980ca2565ac98b
Natalie Hahle <nataliehahle@u.boisestate.edu>
2022-10-11 22:20:03 -0600

    hmmmm

--- main/main.js
+++ main/main.js
@@ -1,7 +1,8 @@
+window.onload = loaded;
 /**
  * Simple Function that will be run when the browser is finished loading.
  */
 function loaded() {
-    var message = "hello!";
+    var message = "Hello world!";
     console.log(message);
 }
--- main/main.ts
+++ main/main.ts
@@ -1,7 +1,9 @@
+window.onload = loaded;
+
 /**
  * Simple Function that will be run when the browser is finished loading.
  */
 function loaded() {
-    let message: string = "hello!"
+    let message: string = "Hello world!"
     console.log(message);
 }
